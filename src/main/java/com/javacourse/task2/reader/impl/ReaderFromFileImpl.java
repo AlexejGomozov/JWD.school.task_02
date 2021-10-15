b7bc6cb6fa7d9ae755a964bb3168c010a5405fd3
AlexejGomozov <alexgomozov1901@gmail.com>
2021-10-15 08:55:44 +0300

    changed file

--- src/main/java/com/javacourse/task2/reader/impl/ReaderFromFileImpl.java
+++ src/main/java/com/javacourse/task2/reader/impl/ReaderFromFileImpl.java
@@ -14,26 +14,22 @@ import java.util.Scanner;
 
 public class ReaderFromFileImpl implements ReaderFromFile {
     static Logger logger = LogManager.getLogger();
-    //private static final String REGEX_SPACE = "\s";
+
     @Override
     public List<String> read(String fileName) throws TriangleException {
-        //public List<String> public String
-        //StringBuilder strBuilder = new StringBuilder();
-        //вместо одной строки создаем лист строк, чтобы эту строку либо провалидировать
+
         List<String> strList = new ArrayList<>();
         try(Scanner scanner = new Scanner(new FileReader(fileName))){
-            //while(scanner.hasNext()) {
+
                  while(scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-               // strBuilder.append(line + REGEX_SPACE);
-                     if(line != null)  {strList.add(line);}
 
+                     if(line != null)  {strList.add(line);}
             }
         } catch (FileNotFoundException e){
             logger.log(Level.ERROR, "File" + fileName + " was not found", e);
             throw  new TriangleException("File was not found", e);
         }
-        //return strBuilder.toString();
         return strList;
     }
 }
